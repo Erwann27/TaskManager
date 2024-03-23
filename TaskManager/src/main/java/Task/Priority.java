a0d1af09eb2893adc451b951d9778a4dadcc24fa
Erwann27 <montagne.erwann.2@gmail.com>
2024-03-23 21:25:16 +0100

    Modified pipeline

--- TaskManager/src/main/java/Task/Priority.java
+++
@@ -1,7 +0,0 @@
-package Task;
-
-public enum Priority {
-    URGENT,
-    NORMAL,
-    MINOR;
-}
--- TaskManager/src/main/java/Task/ProgressiveTask.java
+++
@@ -1,5 +0,0 @@
-package Task;
-
-public interface ProgressiveTask extends SimpleTask{
-    void setProgress(double progress);
-}
